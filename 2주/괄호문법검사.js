2e85ce08bad7664daa89ca8fc041e11c347e18f3
jjabsu <48144688+jjabsu@users.noreply.github.com>
2020-11-11 18:26:01 +0900

    2번까지

--- 2주/괄호문법검사.js
+++ 2주/괄호문법검사.js
@@ -1,57 +1,58 @@
-const inspector = {
-    getDataArr(string) {
-        return string.split('');
-    },
+function getDataArr(string) {
+    return string.split('');
+}
 
-    braceFilter(item) {
-        return item === '[' || item === ']';
-    },
+function braceFilter(item) {
+    return item === '[' || item === ']';
+}
 
-    braceChecker(braceArr) {
-        let stack = [];
-        for (let i of braceArr) {
-            let lastItem = stack[stack.length - 1];
-            if (i === '[') {
-                stack.push(i);
+function valueFilter(item) {
+    return item !== ',' && item !== '[' && item !== ']'
+}
+
+function braceChecker(braceArr) {
+    let stack = [];
+    for (let i of braceArr) {
+        let lastItem = stack[stack.length - 1];
+        if (i === '[') {
+            stack.push(i);
+        } else {
+            if (lastItem === '[') {
+                stack.pop();
             } else {
-                if (lastItem === '[') {
-                    stack.pop();
-                } else {
-                    console.log('여는 괄호가 일치하지 않습니다.');
-                }
+                console.log('여는 괄호가 일치하지 않습니다.');
             }
         }
-        if (stack.length !== 0) {
-            console.log('닫는 괄호가 일치하지 않습니다.');
-        } else {
-            console.log('괄호 매칭이 맞습니다!');
-            return true;
-        }
-    },
-
-    valueFilter(item) {
-        return item !== ',' && item === '[' && item === ']'
-    },
-
-    printInfo(string) {
-        const dataArr = this.getDataArr(string);
-        const braceArr = dataArr.filter(this.braceFilter);
-        const valueArr = dataArr.filter(this.valueFilter);
-        console.log(
-            `깊이 수준은 ${braceArr.length / 2}이며, 총 ${valueArr.length}개의 원소가 포함되어 있습니다.`
-        );
-    },
-    main(string) {
-        const dataArr = this.getDataArr(string);
-        const braceArr = dataArr.filter(this.braceFilter);
-        if (this.braceChecker(braceArr)) {
-            this.printInfo(string);
-        }
     }
-};
+    if (stack.length !== 0) {
+        console.log('닫는 괄호가 일치하지 않습니다.');
+    } else {
+        console.log('괄호 매칭이 맞습니다!');
+        return true;
+    }
+}
+
+
+function printInfo(string) {
+    const dataArr = getDataArr(string);
+    const braceArr = dataArr.filter(braceFilter);
+    const valueArr = dataArr.filter(valueFilter);
+    console.log(
+        `깊이 수준은 ${braceArr.length / 2}이며, 총 ${valueArr.length}개의 원소가 포함되어 있습니다.`
+    );
+}
+
+function main(string) {
+    const dataArr = getDataArr(string);
+    const braceArr = dataArr.filter(braceFilter);
+    if (braceChecker(braceArr)) {
+        printInfo(string);
+    }
+}
+
 
 const data1 = "[1, 2, [3]]";
-inspector.main(data1);
+main(data1);
 
-const data3 = "[1,2,[3,4,[5,[6]]";
-inspector.main(data3);
+const data2 = "[1,2,[3,4,[5,[6]]";
+main(data2);
