7f465ebb90cd3f6d90ba7772973d1cd656a5f649
jjabsu <48144688+jjabsu@users.noreply.github.com>
2020-11-11 18:09:27 +0900

    2번까지

--- 2주/hash/1109월 (Map).js
+++ 2주/hash/1109월 (Map).js
@@ -1,51 +1,51 @@
-Map = function() {
+const Map = function () {
     this.map = new Object();
 };
 
 Map.prototype = {
-    put: function(key, value) {
+    put: function (key, value) {
         this.map[key] = value;
     },
-    get: function(key) {
+    get: function (key) {
         return this.map[key];
     },
-    containsKey: function(key) {
+    containsKey: function (key) {
         return key in this.map;
     },
-    containsValue: function(value) {
-        for (var prop in this.map) {
+    containsValue: function (value) {
+        for (let prop in this.map) {
             if (this.map[prop] == value) return true;
         }
         return false;
     },
-    isEmpty: function(key) {
+    isEmpty: function (key) {
         return (this.size() == 0);
     },
-    clear: function() {
-        for (var prop in this.map) {
+    clear: function () {
+        for (let prop in this.map) {
             delete this.map[prop];
         }
     },
-    remove: function(key) {
+    remove: function (key) {
         delete this.map[key];
     },
-    keys: function() {
-        var keys = new Array();
-        for (var prop in this.map) {
+    keys: function () {
+        let keys = new Array();
+        for (let prop in this.map) {
             keys.push(prop);
         }
         return keys;
     },
-    values: function() {
-        var values = new Array();
-        for (var prop in this.map) {
+    values: function () {
+        let values = new Array();
+        for (let prop in this.map) {
             values.push(this.map[prop]);
         }
         return values;
     },
-    size: function() {
-        var count = 0;
-        for (var prop in this.map) {
+    size: function () {
+        let count = 0;
+        for (let prop in this.map) {
             count++;
         }
         return count;
