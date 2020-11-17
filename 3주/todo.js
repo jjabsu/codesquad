163ca7880e902e43460d1d8f1a046c9cdc7c937c
chc8909 <chc8909@gmailcom>
2020-11-17 11:46:22 +0900

    todo
    css수정

--- 3주/todo.js
+++ 3주/todo.js
@@ -2,29 +2,34 @@ let inputBox = document.querySelector('#inputBox')
 let inputBtn = document.querySelector('#inputBtn')
 let ul = document.querySelector('#ul')
 
-inputBtn.onclick = function() {
+inputBtn.onclick = function () {
 
+    //br
     let newBr = document.createElement('br');
     ul.appendChild(newBr);
 
+    //체크박스
     let newCheckBox = document.createElement('input');
-    newCheckBox.type = 'checkbox'; //속성으로 타입넣기
-    newCheckBox.classList.add("checkBox") //메소드로 클래스명 넣기
+    newCheckBox.type = 'checkbox';
+    newCheckBox.classList.add("checkBox")
     ul.appendChild(newCheckBox);
 
+    //ul에 li태그로 todo넣어주기
     let toDo = inputBox.value;
     let newLi = document.createElement('LI');
-    newLi.classList.add("li") //메소드로 클래스명 넣기
+    newLi.classList.add("li")
     newLi.innerHTML = toDo;
     ul.appendChild(newLi);
-
+    toDo.innerHTML = '';
+    
+    //지우기 버튼 
     let newBtn = document.createElement('BUTTON');
-    newBtn.className = 'deleteBtn' //속성으로 클래스명 넣기
+    newBtn.className = 'deleteBtn'
     newBtn.innerHTML = '지우기'
     ul.appendChild(newBtn)
 
     //체크박스 누르면 글자에 줄추가
-    newCheckBox.onclick = function() {
+    newCheckBox.onclick = function () {
         if (newLi.innerHTML === toDo) {
             newLi.innerHTML = '<del>' + toDo + '</del>'
         } else {
@@ -32,23 +37,19 @@ inputBtn.onclick = function() {
         }
     }
 
-    //지우기버튼 누르면 항목지우기
+    //지우기 
     // newBtn.onclick = function () {
     //     newCheckBox.remove();
     //     newLi.remove();
     //     newBtn.remove();
     // }
 
-
+    //지우기 //addEventListener
     function del() {
+        newBr.remove();
         newCheckBox.remove();
         newLi.remove();
         newBtn.remove();
     }
-
     newBtn.addEventListener("click", del)
 }
-
-
-
-/////////////
