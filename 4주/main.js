aa61eaf23640f2bdc515c4f10c4bcd56f0ad3838
jjabsu <chc8909@gmail.com>
2020-11-24 10:26:29 +0900

    fix
    쓸데없는 코드삭제

--- 4주/main.js
+++ 4주/main.js
@@ -1,24 +1,18 @@
-// SET TIMEOUT (IN MILISECONDS).
 var hide;
 var target;
 
 function showmenu(el) {
     target = document.getElementById(el);
-
     if (target) target.style.display = 'none';
     target.style.display = 'block';
 }
 
-// SET TIME TO HIDE MENU LIST.
 function setTimeToHide() {
-    hide = setTimeout(function() {
+    hide = setTimeout(function () {
         if (target) target.style.display = 'none';
     }, 100);
 }
 
 function ReSetTimer() {
-    if (hide) {
-        clearTimeout(hide);
-        hide = 0;
-    }
+    clearTimeout(hide);
 };
