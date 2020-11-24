4498e4ad77b5f786a60a29f48cf628de1e968933
jjabsu <chc8909@gmail.com>
2020-11-24 10:59:08 +0900

    fix
    리팩토링

--- 4주/main.js
+++ 4주/main.js
@@ -1,18 +1,33 @@
-var hide;
-var target;
 
-function showmenu(el) {
-    target = document.getElementById(el);
-    if (target) target.style.display = 'none';
-    target.style.display = 'block';
+let target;
+let hide;
+
+let title = document.querySelector("#title")
+let list = document.querySelector("#list")
+
+function showmenu() {
+    target = document.getElementById("list")
+    if (target.style.display === 'none') {
+        target.style.display = 'block';
+    }
 }
 
 function setTimeToHide() {
     hide = setTimeout(function () {
-        if (target) target.style.display = 'none';
-    }, 100);
+        if (target.style.display = 'block') {
+            target.style.display = 'none';
+        }
+    }, 1900);
 }
 
 function ReSetTimer() {
     clearTimeout(hide);
 };
+
+title.addEventListener("mouseover", showmenu)
+title.addEventListener("mouseout", setTimeToHide)
+
+
+list.addEventListener("mouseover", ReSetTimer)
+list.addEventListener("mouseout", setTimeToHide)
+
